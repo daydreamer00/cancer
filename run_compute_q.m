ae38c681ff24f732729486a7f0ea68d35750d104
Daydreamer <boxi0418@gmail.com>
2014-07-26 15:32:18 +0800

    change show dir function ls() to dir()

--- run_compute_q.m
+++ run_compute_q.m
@@ -2,11 +2,11 @@ function [ output_args ] = run_compute_q(  )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     dir_name = 'data/';
-    dir_files = ls(dir_name);
+    dir_files = dir(dir_name);
     n_files = size(dir_files,1);
     for i = 1:n_files
         i
-        file_name = dir_files(i,:);
+        file_name = dir_files(i).name;
         if(regexp(file_name,'\.txt\s*$'))
             p_data = load([dir_name file_name]);
             [m, n] = size(p_data);
